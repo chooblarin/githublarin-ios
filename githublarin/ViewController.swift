fbce00f046cddad5f7f66931f39801bdcab1eecd
chooblarin <choo.bla.rin@gmail.com>
2016-01-28 18:08:31 +0900

    Initial Commit

---
+++ githublarin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githublarin
+//
+//  Created by Sota Hatakeyama on 2016/01/28.
+//  Copyright © 2016年 chooblarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
