66064d0f748958529208928667a2c797e107652b
chooblarin <choo.bla.rin@gmail.com>
2016-01-31 22:40:57 +0900

    Show repository data in TableView

---
+++ githublarin/RepositoryTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  RepositoryTableViewCell.swift
+//  githublarin
+//
+//  Created by Sota Hatakeyama on 2016/01/31.
+//  Copyright © 2016年 chooblarin. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var updatedAtLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var starredCountLabel: UILabel!
+    @IBOutlet weak var forksCountLabel: UILabel!
+
+    // MARK: Properties
+
+    var viewModel: Repository?
+    {
+        didSet {
+            guard let viewModel = viewModel else { return }
+            fullNameLabel.text = viewModel.fullName
+            descriptionLabel.text = viewModel.description
+        }
+    }
+}
--- githublarin/ViewController.swift
+++ githublarin/ViewController.swift
@@ -10,27 +10,57 @@ import UIKit
 import RxSwift
 import Gloss
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var repositoryTableView: UITableView!
+
+    // MARK: - Properties
+
+    var repositories = [Repository]()
+    {
+        didSet { self.repositoryTableView.reloadData() }
+    }
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        GitHubAPIClient.sharedInstance.searchRepository("RxJava")
-            .observeOn(Dependencies.sharedDependencies.backgroundWorkScheduler)
-            .subscribeNext { anyobjects -> Void in
-                anyobjects.forEach({ anyobject -> () in
-                    let json = anyobject as! JSON
-                    let repository = Repository(json: json)
-                    print(repository?.fullName)
-                })
-            }.addDisposableTo(self.disposeBag)
+
+        repositoryTableView.dataSource = self
+        loadRepository()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UITableViewDataSource
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifier = "RepositoryTableViewCell"
+        let cell = repositoryTableView.dequeueReusableCellWithIdentifier(identifier) as! RepositoryTableViewCell
+        let repository = repositories[indexPath.row]
+        cell.viewModel = repository
+        return cell
+    }
+
+    func loadRepository() {
+        GitHubAPIClient.sharedInstance.searchRepository("RxJava")
+            .observeOn(Dependencies.sharedDependencies.backgroundWorkScheduler)
+            .subscribeNext { anyobjects -> Void in
+                anyobjects.map({ anyobject -> Repository? in
+                    let json = anyobject as! JSON
+                    return Repository(json: json)
+                }).forEach({ repository -> () in
+                    if let repository = repository {
+                        self.repositories.append(repository)
+                    }
+                })
+            }.addDisposableTo(self.disposeBag)
+    }
 }
