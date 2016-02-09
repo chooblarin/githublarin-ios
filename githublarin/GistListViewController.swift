f3a6b34742be39ed56de995e1a5e0ee5bd83250b
chooblarin <choo.bla.rin@gmail.com>
2016-02-09 13:55:00 +0900

    Add SegmentedControll to switch MyGist to StarredGist

--- githublarin/GistListViewController.swift
+++ githublarin/GistListViewController.swift
@@ -1,58 +1,34 @@
 import UIKit
 import RxSwift
 
-class GistListViewController: UIViewController, UITableViewDataSource {
-
-    // MARK: - Properties
-
-    var gists = [Gist]() {
-        didSet {
-            tableView.reloadData()
-        }
-    }
-    let apiClient = GitHubAPIClient.sharedInstance
-    let disposeBag = DisposeBag()
-    let cellIdentifier = "GistCell"
+class GistListViewController: UIViewController {
 
     // MARK: - IBOutlets
-
-    @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet weak var myGistsContainer: UIView!
+    @IBOutlet weak var starredGistsConainer: UIView!
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.dataSource = self
-        tableView.estimatedRowHeight = 100.0
-        tableView.rowHeight = UITableViewAutomaticDimension
-
-        apiClient.gists().subscribeNext {
-            self.gists = $0
-        }.addDisposableTo(disposeBag)
-    }
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if "ShowGistDetail" == segue.identifier {
-            let navigationController = segue.destinationViewController as! UINavigationController
-            let detailWebViewController = navigationController.viewControllers.first as! DetailWebViewController
-            let gistCell = sender as! GistCell
-            detailWebViewController.gist = gistCell.gist
-        }
-    }
-
-    // MARK: - UITableViewDataSource
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gists.count
-    }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! GistCell
-        cell.gist = gists[indexPath.row]
-        return cell
     }
 
     func selected() {
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func changeSegment(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: // My Gist
+            myGistsContainer.hidden = false
+            starredGistsConainer.hidden = true
+            
+        case 1: // Starred Gist
+            myGistsContainer.hidden = true
+            starredGistsConainer.hidden = false
+
+        default: break
+        }
     }
 }
---
+++ githublarin/GistsViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import RxSwift
+
+class GistsViewController: UIViewController, UITableViewDataSource {
+
+    // MARK: - Properties
+    
+    var gists = [Gist]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    let apiClient = GitHubAPIClient.sharedInstance
+    let disposeBag = DisposeBag()
+    let cellIdentifier = "GistCell"
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 100.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        apiClient.gists().subscribeNext { self.gists = $0 }.addDisposableTo(disposeBag)
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if "ShowGistDetail" == segue.identifier {
+            let navigationController = segue.destinationViewController as! UINavigationController
+            let detailWebViewController = navigationController.viewControllers.first as! DetailWebViewController
+            let gistCell = sender as! GistCell
+            detailWebViewController.gist = gistCell.gist
+        }
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gists.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! GistCell
+        cell.gist = gists[indexPath.row]
+        return cell
+    }
+}
